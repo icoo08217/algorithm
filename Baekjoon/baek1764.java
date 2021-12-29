b785d36da7fd8d88c32681b6791351d11ecffcac
byungchan <icoo08216@gmail.com>
2021-12-29 20:08:39 +0900

    21.12.29

---
+++ Baekjoon/baek1764.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(str.nextToken());
+        int M = Integer.parseInt(str.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i=0; i<N; i++){
+            String d = br.readLine();
+            set.add(d);
+        }
+
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i=0; i<M; i++){
+            String bo = br.readLine();
+            if (set.contains(bo)){
+                answer.add(bo);
+            }
+        }
+
+        Collections.sort(answer);
+
+        System.out.println(answer.size());
+        for (String s : answer) {
+            System.out.println(s);
+        }
+
+
+    }
+}
