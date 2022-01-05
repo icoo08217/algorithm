2143554de852cc25f6849b2206d4c06793d81d09
byungchan <icoo08216@gmail.com>
2022-01-05 20:14:29 +0900

    22.01.05

---
+++ Baekjoon/baek9093.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        Stack<Character> word = new Stack<>();
+
+        for(int i=0; i<T; i++){
+            String s = br.readLine() + "\n";
+            for (char c : s.toCharArray()) {
+                if (c == ' ' || c == '\n'){
+                    while (!word.isEmpty()){
+                        bw.write(word.pop());
+                    }
+                    bw.write(c);
+                }
+
+                else {
+                    word.push(c);
+                }
+            }
+        }
+        bw.flush();
+    }
+}
