6e59d174a0f901b420fa85b9e20801f716e3e28a
byungchan <icoo08216@gmail.com>
2022-01-13 17:13:50 +0900

    22.01.13

---
+++ Baekjoon/baek10808.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int count[] = new int[26]; // 각 알파벳을 세어줄 행렬
+
+        String input = br.readLine();
+
+        for (int i=0; i< input.length(); i++){
+            char c = input.charAt(i);
+            // 'a' 의 인트형 수는 97이다.
+            count[c - 97]++;
+        }
+
+        for(int cnt : count){
+            sb.append(cnt).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
+
+
