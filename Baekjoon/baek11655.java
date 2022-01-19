9a018ed5a44eb118b33c65bb6267863b8a2394e0
byungchan <icoo08216@gmail.com>
2022-01-19 21:07:50 +0900

    22.01.19

---
+++ Baekjoon/baek11655.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        int temp;
+
+        for (int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+
+            if (c >= 65 && c <= 90) { // 'A' ~ 'Z' 일 경우
+                temp = c + 13;
+                if (temp >90) {
+                    temp = temp - 'Z' + 'A' - 1;
+                    sb.append((char)temp);
+                } else {
+                    sb.append((char)temp);
+                }
+            }
+            else if (c >= 'a' && c <= 'z'){ // 'a' ~ 'z' 일 경우
+                temp = c + 13;
+                if (temp > 'z'){
+                    temp = temp - 'z' + 'a' - 1;
+                    sb.append((char)temp);
+                } else {
+                    sb.append((char)temp);
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
---
+++ Baekjoon/baek11656.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        String arr[] = new String[input.length()];
+
+        for (int i=0; i<input.length(); i++){
+            arr[i] = input.substring(i);
+        }
+
+        Arrays.sort(arr);
+
+        for (int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
