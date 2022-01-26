8511941a286565d4b74cf898a15d03cb44bca8c2
byungchan <icoo08216@gmail.com>
2022-01-26 19:45:16 +0900

    22.01.26

---
+++ Baekjoon/baek1978.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+
+        for (int i=0; i<N; i++){
+            int decimal = Integer.parseInt(st.nextToken());
+
+            if (Decimal(decimal)) count++;
+        }
+
+        System.out.println(count);
+
+    }
+
+    static boolean Decimal(int n){
+
+        boolean flag = true;
+
+        if (n == 1) { // 1이 들어오면 소수가 아니다.
+            return false;
+        }
+        else {
+
+            for (int i =2; i<n; i++){
+                if (n % i == 0) {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+    }
+}
+
+
