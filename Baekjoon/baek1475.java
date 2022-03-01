1c519f28264ce90854e6b18bf81a73ad700e235b
byungchan <icoo08216@gmail.com>
2022-03-01 22:44:05 +0900

    22.03.01

---
+++ Baekjoon/baek1475.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+
+        String roomNum = br.readLine(); // 방번호
+        int[] numSet = new int[10];
+        for (char c : roomNum.toCharArray()) {
+            int num = c -'0';
+            if (num == 9) {
+                num = 6;
+            }
+            numSet[num]++;
+        }
+
+        numSet[6] = numSet[6]/2 + numSet[6]%2;
+
+        Arrays.sort(numSet);
+
+        System.out.println(numSet[9]);
+
+    }
+}
