282dc13d7bb3a538b7780cb77d73210eb6b9676a
byungchan <icoo08216@gmail.com>
2022-03-22 23:57:20 +0900

    22.03.22

---
+++ Baekjoon/baek11724.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    static int n , m;
+    static int check[][];
+    static boolean visited[];
+    static int cnt=0;
+    static int u,v ;
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        check = new int[1001][1001];
+        visited = new boolean[1001];
+
+
+        for (int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            check[u][v] = check[v][u] = 1;
+        }
+
+        for (int i =1; i<=n; i++){
+            if (visited[i] == false){
+                dfs(i);
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+
+    }
+
+    public static void dfs(int i){
+
+        if (visited[i] == true){
+            return;
+        }
+        else {
+            visited[i] = true;
+
+            for (int j=1; j<=n; j++){
+                if (check[i][j] == 1){
+                    dfs(j);
+                }
+            }
+        }
+
+    }
+}
