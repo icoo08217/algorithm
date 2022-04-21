54a8a88c9ea0065355a5557320a9ab31dfeb1801
byungchan <icoo08216@gmail.com>
2022-04-21 22:32:13 +0900

    22.04.21

---
+++ Baekjoon/baek1013.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T --> 0){
+            String str = br.readLine();
+            String pattern = "(100+1+|01)+";
+            System.out.println(str.matches(pattern)? "YES" : "NO");
+        }
+    }
+}
