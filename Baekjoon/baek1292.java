5686117c6b448582b169f94719ea449a5b2d4204
byungchan <icoo08216@gmail.com>
2022-04-24 14:21:06 +0900

    22.04.24/구현

---
+++ Baekjoon/baek1292.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static StringBuilder sb;
+    public static int index , sum;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[1001];
+        index = 1;
+
+        // 수열 채워넣기
+        for (int i = 1; i<=1000; i++){
+            for (int j=1; j<=i; j++) {
+                if (index > 1000) break;
+                arr[index] = i;
+                index++;
+            }
+        }
+        // 결과 값 도출
+        sum = 0;
+        for (int i = A; i<=B; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
