f318d8207e11e4a145184a5f7c2cf3952d70c7a5
byungchan <icoo08216@gmail.com>
2022-05-30 19:58:41 +0900

    22.05.30/패션왕 신해빈(해쉬맵)

---
+++ Baekjoon/baek9375.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            HashMap<String , Integer> fashion = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine() ," ");
+                st.nextToken();
+                String kind = st.nextToken();
+                if (fashion.containsKey(kind)){
+                    fashion.put(kind , fashion.get(kind) + 1);
+                } else {
+                    fashion.put(kind, 1);
+                }
+            }
+            int result = 1;
+            for (int val : fashion.values()) {
+                result *= (val+1);
+            }
+            System.out.println(result - 1);
+        }
+    }
+
+}
