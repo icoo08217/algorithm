a5eaa7c1be0eebdca02b1565a488fc5871204e9a
byungchan <icoo08216@gmail.com>
2022-06-16 15:02:50 +0900

    22.06.16/정렬

---
+++ Baekjoon/baek1181(단어 정렬).java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
+            arr[i] = temp;
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()){ // 길이가 같다면 사전순으로 정렬하여야 한다.
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        System.out.println(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
