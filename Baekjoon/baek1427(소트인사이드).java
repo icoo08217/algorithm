257f27654b56d55a5d936ffced9f01fd4ef716d6
byungchan <icoo08216@gmail.com>
2022-06-16 17:07:24 +0900

    22.06.16/정렬

---
+++ Baekjoon/baek1427(소트인사이드).java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String num = br.readLine();
+        Integer[] arr = new Integer[num.length()];
+
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            arr[i] = c - 48;
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i : arr) {
+            System.out.print(i);
+        }
+
+    }
+}
