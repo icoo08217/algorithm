bc5fc36b6759ae6637036686ebce129bd6477ec2
icoo08217 <icoo08216@gmail.com>
2022-11-04 17:43:38 +0900

    [PG-Lv2] 양궁대회 - 주석 변경

--- programmars/kakao2022/양궁대회.java
+++ programmars/kakao2022/양궁대회.java
@@ -18,11 +18,14 @@ class Solution_양궁대회{
     public int[] solution(int n, int[] info) {
 
         int[] answer = new int[11]; // 정답을 저장할 배열
-        int[] temp = new int[11]; // 정답이 저장되어 있는 배열과 비교할 배열
+        int[] temp = new int[11]; // 정답이 저장되어 있는 배열과 비교할 임시 배열
         int maxDiff = 0; // 라이언 총합과 어피치 총합의 차이를 저장할 변수
 
+        // 비트형으로 된 부분집합을 가지고 모든 경우의 수를 만들어 내고 , 모두 확인 (완전 탐색)
+        // 1 << 10 ==> 1 * 2^(10) 이다.  = 1024
+        // subset ==> 0 0 0 0 0 0 0 0 0 1
+        // 0 : 라이언 패 , 1 : 라이언 승
         for (int subset = 1; subset < (1 << 10); ++subset) {
-            // 비트마스크 사용 (1 << 10) => 1024
             int ryan = 0 , appeach = 0 , count = 0; // 라이언 총합 , 어피치 총합 , 라이언이 사용한 화살의 갯수
             for (int i = 0; i < 10; ++i) {
                 if ((subset & ( 1 << i)) != 0) { // 1 이라면 라이언이 승리
@@ -31,13 +34,14 @@ class Solution_양궁대회{
                     count += temp[i];
                 } else { // 0 이라면 어피치가 승리 or 비김
                     temp[i] = 0; // 라이언은 화살을 하나도 사용하지 않음.
-                    if (info[i] > 0) {
+                    if (info[i] > 0) { // 어피치가 점수를 가져가야하는 경우
                         appeach += 10 - i;
                     }
                 }
             }
 
             if ( count > n) continue; // 라이언이 화살을 n 개 보다 많이 사용했을 경우는 continue
+
             temp[10] = n - count; // 라이언의 남은 화살의 개수는 모두 0점 짜리 과녘을 맞췄다고 가정
 
             if (ryan - appeach == maxDiff) { // 여러가지 방법이 나올 경우
---
+++ programmars/lv1/푸드파이트대회.java
@@ -0,0 +1,20 @@
+package programmers.lv1;
+
+public class 푸드파이트대회 {
+    public static void main(String[] args) {
+        Solution_푸드파이트대회 solution = new Solution_푸드파이트대회();
+        int[] food = new int[] {1, 3, 4, 6};
+        System.out.println(solution.solution(food));
+    }
+}
+
+class Solution_푸드파이트대회{
+    public String solution(int[] food) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+}
+
