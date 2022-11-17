1758b1072f2ac1715bc3ae8dd518bfb8113a6138
icoo08217 <icoo08216@gmail.com>
2022-11-17 22:13:57 +0900

    [PG] Lv2 - 후보키

---
+++ programmars/lv2/후보키.java
@@ -0,0 +1,14 @@
+package programmers.lv2;
+
+public class 후보키 {
+    public static void main(String[] args) {
+        System.out.println("후보키 문제입니다.");
+    }
+}
+
+class Solution_후보키{
+    public int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+}
